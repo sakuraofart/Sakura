ce9a53665289e9da20f5faa352c523e4930c5d2d
sakuraofart <sakura.webstudio@gmail.com>
2024-07-30 20:08:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+class Writer {
+    constructor(node) {
+      this.node = node;
+      
+      if (!this.node) return;
+  
+      this.timer = 150; // .2s
+      this.broken = this.node.textContent.split('');
+  
+      this._init();
+    }
+  
+    _init() {
+      this.node.textContent = '';
+      let i = 0;
+  
+      let interval = setInterval(() => {
+        this.node.textContent += this.broken[i];
+  
+        i++;
+  
+        if (i >= this.broken.length) clearInterval(interval);
+      }, this.timer);
+    }
+  }
+  
+  const root = document.querySelector('.title');
+  
+  new Writer(root);
