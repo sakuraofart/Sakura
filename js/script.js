7ee7f754a94409e2272f64f0583dd5897b5a7984
Profisional-l <vanjasavsjuk@gmail.com>
2024-08-09 14:54:09 +0300

    mobile adapt

--- js/script.js
+++ js/script.js
@@ -1,188 +1,229 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-class Writer {
+  class Writer {
     constructor(node) {
       this.node = node;
-      
+
       if (!this.node) return;
-  
+
       this.timer = 52; // .2s
-      this.broken = this.node.textContent.split('');
-  
+      this.broken = this.node.textContent.split("");
+
       this._init();
     }
-  
+
     _init() {
-      this.node.textContent = '';
+      this.node.textContent = "";
       let i = 0;
-  
+
       let interval = setInterval(() => {
         this.node.textContent += this.broken[i];
-  
+
         i++;
-  
+
         if (i >= this.broken.length) clearInterval(interval);
       }, this.timer);
     }
   }
-  
-  const root = document.querySelector('.title');
-  
+
+  const root = document.querySelector(".title");
+
   new Writer(root);
 
+  //ANIMATION BACKGROUND VIDEO
+  const video = document.getElementById("backgroundVideo");
+  let lastScrollTop = 0;
 
-//ANIMATION BACKGROUND VIDEO
-  const video = document.getElementById('backgroundVideo');
-    let lastScrollTop = 0;
+  window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
-    window.addEventListener('scroll', () => {
-        const scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-        if (scrollTop > lastScrollTop) {
-            // Скроллим вниз
-            video.play();
-        } else {
-            // Скроллим вверх
-            video.currentTime -= 0.001; // Уменьшаем время на 0.1 секунды
-        }
-
-        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Для мобильных устройств или если скроллим вверх
-    });
-
-    const portfolioItems = document.querySelectorAll(".portfolio__item");
-
-    portfolioItems.forEach((item) => {
-        const video = item.querySelector(".videoBGportfolio");
-        
-        item.addEventListener("mouseenter", () => {
-            if (video) {
-                video.play();
-            }
-        });
-    
-        item.addEventListener("mouseleave", () => {
-            if (video) {
-                video.pause();
-                video.currentTime = 0; // Сброс видео на начало
-            }
-        });
-    });
-
-
-    const menu = document.querySelector(".menu");
-    const verticalMenu = document.querySelector(".vertical-menu");
-    const menuStartHTML = menu.innerHTML;
-    const header = document.querySelector('header');
-
-    // Функция для обновления меню в зависимости от ширины экрана
-function updateMenu() {
-
-  // Проверка текущей ширины экрана
-  if (window.innerWidth < 950) {
-    const menuBurgerHTML = "<img class='menu-burger' style='margin-top: 12px' width='20px' src='./images/AllAssets/hamburger.png'>";
-    const closeCrossHTML = "<img class='menu-close' style='margin-top: 12px' width='18px' src='./images/AllAssets/Crossclose.png'>";
-
-    // Устанавливаем HTML для меню
-    menu.innerHTML = menuBurgerHTML;
-
-    // Добавляем обработчик клика
-    menu.addEventListener("click", function toggleMenu() {
-      if (header.classList.contains("opened")) {
-        header.classList.remove("opened");
-        header.classList.add("closed");
-        verticalMenu.classList.remove("menuIsOpen");
-        verticalMenu.classList.add("menuIsClose");
-        enableScroll();
-        menu.innerHTML = menuBurgerHTML;
-      } else {
-        header.classList.remove("closed");
-        header.classList.add("opened");
-        verticalMenu.classList.add("menuIsOpen");
-        verticalMenu.classList.remove("menuIsClose");
-        disableScroll();
-        menu.innerHTML = closeCrossHTML;
-      }
-    });
-    menu.style.marginTop = "0px";
-
-  } else {
-    // Если ширина экрана больше или равна 850px, изменяем стили
-    menu.style.marginTop = "5px";
-    menu.innerHTML = menuStartHTML;
-  }
-}
-
-// Функции для блокировки и разблокировки прокрутки
-function disableScroll() {
-  document.body.style.overflow = 'hidden';
-}
-
-function enableScroll() {
-  document.body.style.overflow = '';
-}
-
-// Инициализация меню при загрузке страницы
-updateMenu();
-
-// Обработчик события resize для обновления меню при изменении размера окна
-window.addEventListener('resize', updateMenu);
-
-
-// --------------------------------- Start Portfolio Mobile Animation---------------------------------
-if (window.innerWidth < 900) {
-  const items = document.querySelectorAll('.portfolio__item');
-  let index = 0;
-
-  function triggerAnimation() {
-    // Убираем предыдущий активный класс, если это не первый элемент
-    if (index > 0) {
-      const prevVideo = items[index - 1].querySelector('.videoBGportfolio');
-      items[index - 1].classList.remove('hover');
-      if (prevVideo) {
-        prevVideo.pause(); // Останавливаем предыдущее видео
-      }
-    }
-
-    // Добавляем активный класс для текущего элемента
-    const currentItem = items[index];
-    const currentVideo = currentItem.querySelector('.videoBGportfolio');
-    currentItem.classList.add('hover');
-
-    if (currentVideo) {
-      currentVideo.currentTime = 0; // Сбрасываем видео к началу
-      currentVideo.play(); // Воспроизводим видео
-    }
-
-    // Увеличиваем индекс
-    index++;
-
-    // Проверяем, достигли ли мы конца списка элементов
-    if (index < items.length) {
-      // Если нет, запускаем анимацию для следующего блока через 3 секунды
-      setTimeout(triggerAnimation, 7000);
+    if (scrollTop > lastScrollTop) {
+      // Скроллим вниз
+      video.play();
     } else {
-      // Если да, то сбрасываем анимацию и начинаем заново через 3 секунды
-      setTimeout(() => {
-        const lastVideo = items[items.length - 1].querySelector('.videoBGportfolio');
-        items[items.length - 1].classList.remove('hover');
-        if (lastVideo) {
-          lastVideo.pause(); // Останавливаем последнее видео
+      // Скроллим вверх
+      video.currentTime -= 0.001; // Уменьшаем время на 0.1 секунды
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Для мобильных устройств или если скроллим вверх
+  });
+
+  const portfolioItems = document.querySelectorAll(".portfolio__item");
+
+  portfolioItems.forEach((item) => {
+    const video = item.querySelector(".videoBGportfolio");
+
+    item.addEventListener("mouseenter", () => {
+      if (video) {
+        video.play();
+      }
+    });
+
+    item.addEventListener("mouseleave", () => {
+      if (video) {
+        video.pause();
+        video.currentTime = 0; // Сброс видео на начало
+      }
+    });
+  });
+
+  const menu = document.querySelector(".menu");
+  const verticalMenu = document.querySelector(".vertical-menu");
+  const menuStartHTML = menu.innerHTML;
+  const header = document.querySelector("header");
+
+  // Функция для обновления меню в зависимости от ширины экрана
+  function updateMenu() {
+    // Проверка текущей ширины экрана
+    if (window.innerWidth < 950) {
+      const menuBurgerHTML =
+        "<img class='menu-burger' style='margin-top: 12px' width='20px' src='./images/AllAssets/hamburger.png'>";
+      const closeCrossHTML =
+        "<img class='menu-close' style='margin-top: 12px' width='18px' src='./images/AllAssets/Crossclose.png'>";
+
+      // Устанавливаем HTML для меню
+      menu.innerHTML = menuBurgerHTML;
+
+      // Добавляем обработчик клика
+      menu.addEventListener("click", function toggleMenu() {
+        if (header.classList.contains("opened")) {
+          header.classList.remove("opened");
+          header.classList.add("closed");
+          verticalMenu.classList.remove("menuIsOpen");
+          verticalMenu.classList.add("menuIsClose");
+          enableScroll();
+          menu.innerHTML = menuBurgerHTML;
+        } else {
+          header.classList.remove("closed");
+          header.classList.add("opened");
+          verticalMenu.classList.add("menuIsOpen");
+          verticalMenu.classList.remove("menuIsClose");
+          disableScroll();
+          menu.innerHTML = closeCrossHTML;
         }
-        index = 0; // Сбрасываем индекс
-        triggerAnimation(); // Запускаем цикл заново
-      }, 3000);
+      });
+      menu.style.marginTop = "0px";
+    } else {
+      // Если ширина экрана больше или равна 850px, изменяем стили
+      menu.style.marginTop = "5px";
+      menu.innerHTML = menuStartHTML;
     }
   }
 
-  // Запускаем цикл анимации
-  triggerAnimation();
-}
-// --------------------------------- END Portfolio Mobile Animation---------------------------------
+  // Функции для блокировки и разблокировки прокрутки
+  function disableScroll() {
+    document.body.style.overflow = "hidden";
+  }
 
+  function enableScroll() {
+    document.body.style.overflow = "";
+  }
 
+  // Инициализация меню при загрузке страницы
+  updateMenu();
 
+  // Обработчик события resize для обновления меню при изменении размера окна
+  window.addEventListener("resize", updateMenu);
 
+  // --------------------------------- Start Portfolio Mobile Animation---------------------------------
+  if (window.innerWidth < 900) {
+    const items = document.querySelectorAll(".portfolio__item");
+    let index = 0;
 
+    items.forEach((e) => {
+      e.addEventListener("click", () => {
+        e.classList.add("activated");
+      });
+    });
 
+    // items.forEach((element) => {
+    //   if (!element.matches(':hover')) {
+    //     console.log("dwddw")
+    //   }
+    // })
 
-})
+    // setInterval(() => {
+    //   for (var i = 0; i < items.length; i++) {
+    //       if (!items[i].matches(':hover')) {
+    //         console.log("no in ", i)
+    //       }
+
+    //   }
+    // }, 3500)
+
+    const target = document.querySelector(".portfolio");
+
+    function checkScroll() {
+      
+      const targetOffsetTop = target.offsetTop;
+      const windowScrollTop = window.scrollY;
+      const windowHeight = window.innerHeight;
+
+      // Проверяем, виден ли элемент
+      if (
+        targetOffsetTop < windowScrollTop + windowHeight &&
+        targetOffsetTop + target.offsetHeight > windowScrollTop
+      ) {
+        console.log("Элемент виден!");
+
+        function triggerAnimation() {
+          // Убираем предыдущий активный класс, если это не первый элемент
+          if (index > 0) {
+            const prevVideo =
+              items[index - 1].querySelector(".videoBGportfolio");
+            items[index - 1].classList.remove("hover");
+            if (prevVideo) {
+              prevVideo.pause(); // Останавливаем предыдущее видео
+            }
+          }
+
+          // Добавляем активный класс для текущего элемента
+          const currentItem = items[index];
+          const currentVideo = currentItem.querySelector(".videoBGportfolio");
+          currentItem.classList.add("hover");
+
+          if (currentVideo) {
+            currentVideo.currentTime = 0; // Сбрасываем видео к началу
+            currentVideo.play(); // Воспроизводим видео
+          }
+
+          // Увеличиваем индекс
+          index++;
+
+          // Проверяем, достигли ли мы конца списка элементов
+          if (index < items.length) {
+            // Если нет, запускаем анимацию для следующего блока через 3 секунды
+            setTimeout(triggerAnimation, 3250);
+          } else {
+            // Если да, то сбрасываем анимацию и начинаем заново через 3 секунды
+            setTimeout(() => {
+              const lastVideo =
+                items[items.length - 1].querySelector(".videoBGportfolio");
+              items[items.length - 1].classList.remove("hover");
+              if (lastVideo) {
+                lastVideo.pause(); // Останавливаем последнее видео
+              }
+              index = 0; // Сбрасываем индекс
+              triggerAnimation(); // Запускаем цикл заново
+            }, 3000);
+          }
+        }
+        setTimeout(() => {
+          triggerAnimation()
+        }, 1000)
+        window.removeEventListener("scroll", checkScroll);
+      }
+      
+    }
+
+    window.addEventListener("scroll", checkScroll);
+
+    // setTimeout(
+
+    // }, 5000);
+
+    // Запускаем цикл анимации
+    // setTimeout(  triggerAnimation(), 7000)
+  }
+
+  // --------------------------------- END Portfolio Mobile Animation---------------------------------
+});
